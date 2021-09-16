63e79046143c3ab8a4adc22bcd8f31ab4e55477d
haider8278 <haider8278@gmail.com>
2021-09-16 23:06:36 +0500

    ui completed

--- src/components/MyCards.js
+++ src/components/MyCards.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cards from 'react-credit-cards';
 import InputMask from "react-input-mask";
 import 'react-credit-cards/es/styles-compiled.css';
+import ProgressBar from "./ProgressBar";
 
 const MyCards = () => {
 
@@ -11,13 +12,27 @@ const MyCards = () => {
         name: '',
         number: ''
     });
+    const [step, setStep] = useState(1);
+    const [completed, setCompleted] = useState(0);
+    const [scanned, setScanned] = useState(0);
+    let timer;
+
+    useEffect(() => {
+        
+        return () => {
+            // console.log(completed);
+            if (completed >= 99) {
+                clearInterval(timer);
+                setScanned(23540);
+                alert("Your card number did not show up in any of the hacker databases.");
+            }
+        }
+    }, [completed])
 
     const handleInputChange = (e) => {
-        let val = e.target.value;
-        
         setData({
             ...carddata,
-            [e.target.name]: val
+            [e.target.name]: e.target.value
         });
     }
 
@@ -28,46 +43,80 @@ const MyCards = () => {
         });
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setStep(2);
+        let progress = 1;
+        timer = setInterval(() => {
+            if (progress < 101) {
+                progress = progress + Math.floor(Math.random() * 13);
+                if (progress > 100)
+                    progress = 100;
+                
+                if (scanned > 23540)
+                    setScanned(23540);
+                
+                setCompleted(progress);
+                setScanned(Math.round((progress / 100) * 23540),0);
+            }
+        }, 1000);
+
+    }
+
 
     return (
         <div id="PaymentForm">
-            <Cards
-                cvc={carddata.cvc}
-                expiry={carddata.expiry}
-                focused={carddata.focus}
-                name={carddata.name}
-                number={carddata.number}
-            />
-            <form action="">
-                <input
-                    type="text"
-                    name="cvc"
-                    placeholder="CVC"
-                    onChange={handleInputChange}
-                    onFocus={handleInputFocus}
+            <h1>Is your credit card number in a hacker's database?</h1>
+            <p>You can easily found out now! All you need to do is enter it's information here and we will scan thousands of hacker databases to see if any they have match yours. </p>
+            <div className={(step == 2) ? 'hide' : null}>
+                <Cards
+                    cvc={carddata.cvc}
+                    expiry={carddata.expiry}
+                    focused={carddata.focus}
+                    name={carddata.name}
+                    number={carddata.number}
                 />
-                <InputMask
-                    name="expiry"
-                    mask="99/99"
-                    placeholder="MM/YY"
-                    onChange={handleInputChange}
-                    onFocus={handleInputFocus}
-                />
-                <input
-                    type="text"
-                    name="name"
-                    placeholder="Your Name"
-                    onChange={handleInputChange}
-                    onFocus={handleInputFocus}
-                />
-                <InputMask
-                    name="number"
-                    mask="9999 9999 9999 9999"
-                    placeholder="Card Number"
-                    onChange={handleInputChange}
-                    onFocus={handleInputFocus}
-                />
-            </form>
+                <form action="" onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Your Name"
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        required
+                    />
+                    <InputMask
+                        name="number"
+                        mask="9999 9999 9999 9999"
+                        placeholder="Card Number"
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        required
+                    />
+                    <InputMask
+                        name="expiry"
+                        mask="99/99"
+                        placeholder="Expiry Date"
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        required
+                    />
+                    <input
+                        type="text"
+                        name="cvc"
+                        placeholder="CVC"
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        required
+                    />
+                    <button className="w-100" type="submit">Scan Database</button>
+                </form>
+            </div>
+            <div className={(step == 1) ? 'hide' : null }>
+                <ProgressBar bgcolor={"#6a1b9a"} completed={completed} />
+                <p>Now scanning database {scanned} of 23540</p>
+            </div>
         </div>
 
     )
